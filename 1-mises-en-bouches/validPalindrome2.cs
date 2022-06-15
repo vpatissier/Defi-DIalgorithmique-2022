9c9cac8caf86588cb828a994e127f6942848c120
Etienne Marcoux <emarcoux@dialoginsight.com>
2022-06-15 13:51:57 -0400

    palindrome

--- 1-mises-en-bouches/validPalindrome2.cs
+++ 1-mises-en-bouches/validPalindrome2.cs
@@ -1,5 +1,27 @@
 public class Solution {
+    
     public bool ValidPalindrome(string s) {
+        if (isPalindrome(s)) return true;
         
+        for (int i = 0; i < s.Length; i++)
+        {
+            string copy = s.Clone();
+            string reducedString = copy.Remove(i);
+            return isPalindrome(reducedString);
+        }
+    }
+
+    private bool isPalindrome(string s)
+	{
+        char[] charArray = s.ToCharArray();
+        Array.Reverse(charArray);
+        string reversed = new string(charArray);
+
+        return s.Equals(reversed);
+    }
+
+    static void Main()
+    {
+        bool valid = ValidPalindrome("abca");
     }
 }
